bb03d8e3e78815f802a239a00fbd2091a118ccb2
ranveerm <ranveer.mam@gmail.com>
2021-12-27 11:31:43 +1100

    ➕ Added object to encapsulate standalone number theory algorithms

---
+++ Sources/SwfitCryptography/Mathematics/Number Theory/NumberTheory.swift
@@ -0,0 +1,14 @@
+//
+//  Number Theory
+//  
+//
+//  Created by ranveerm on 27/12/21.
+//
+
+import Foundation
+
+/**
+ Caseless `enum` encapsulating number theory algorithms that can be implemented as standalone + pure functions (alleviating the need to create an object, when encapsulation of a state is not necessary).
+ - Author: 👨🏾‍💻 [ranveerm](https://ranveerm.com/about)
+ */
+public enum NumberTheory { }
