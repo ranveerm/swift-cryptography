73442d9c9f081aa1d0a3e8bf28e975036b9de895
ranveerm <ranveer.mam@gmail.com>
2021-12-30 19:25:08 +1100

    🪄 Created API for modulo multiplicative inverse

---
+++ Tests/SwfitCryptographyTests/Mathematics/Number Theory/Modular Multiplicative Inverse Tests.swift
@@ -0,0 +1,43 @@
+//
+//  Modular Multiplicative Inverse Tests.swift
+//  
+//
+//  Created by Ranveer Mamidpelliwar on 29/12/21.
+//
+
+import XCTest
+import SwfitCryptography
+
+class Modular_Multiplicative_Inverse_T: XCTestCase {
+    func test_error() throws {
+        /// Given
+        let b = 6
+        let base = 10
+        
+        let expectedError = NumberTheory.ModuloArithmaticError.inputIsNotCoprimeWithModuloBase
+        
+        /// When
+        XCTAssertThrowsError(try b.multiplicativeInverse(modulo: base), "Error not thrown when input is not coprime with modulo base") { error in
+            /// Then
+            guard let computedError = error as? NumberTheory.ModuloArithmaticError else {
+                XCTFail("Incorrect error thrown")
+                return
+            }
+            XCTAssertEqual(expectedError, computedError)
+        }
+    }
+    
+    func test_computeInverse() throws {
+        /// Given
+        let b = 3
+        let base = 20
+        let expectedModuloInverse = 7
+        
+        /// When
+        let computedModuloInverse = try b.multiplicativeInverse(modulo: base)
+        
+        /// Then
+        XCTAssertEqual(expectedModuloInverse, computedModuloInverse)
+        XCTAssertEqual((b * expectedModuloInverse) % base, 1)
+    }
+}
