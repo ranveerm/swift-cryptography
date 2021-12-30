aa29873176a1584a941e12935c2c37e26457903b
ranveerm <ranveer.mam@gmail.com>
2021-12-30 20:02:03 +1100

    ➕ Added resources used within project

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwfitCryptography",
-            dependencies: []),
+            dependencies: [],
+            resources: [.copy("Resources")]),
         .testTarget(
             name: "SwfitCryptographyTests",
             dependencies: ["SwfitCryptography"]),
